77ca3bda646a626f2415e1e425a10c4c0930e556
Elwa36s <24dds08@gmail.com>
2020-10-18 17:45:59 +0300

    feat: finish all checks

--- src/index.js
+++ src/index.js
@@ -1,33 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-let string = str.split(''),
-    stack = [],
-    openingBracket = [],
-    closingBracket = [],
-    openSymb,
-    closeSymb;
-for (let i = 0; i < bracketsConfig.length; i++) {
-    let symb = bracketsConfig[i];
-    openingBracket.push(symb[0]);
-    closingBracket.push(symb[1]);
-}
-console.log('open: ' + openingBracket);
-console.log('close: ' + closingBracket);
-for (let i = 0; i < string.length; i++) {
-  openSymb = openingBracket.indexOf(string[i]);
-  if (openSymb !== -1) {
-    stack.push(openSymb);
-    //continue;
-  }
-  closeSymb = closingBracket.indexOf(string[i]);
-  if (closeSymb !== -1) {
-    openSymb = stack.pop();
-    if (closeSymb !== openSymb) {
+    let string = str,
+        openingBracket = [],
+        closingBracket = [],
+        openSymb,
+        closeSymb;
+    const stack = [];
+     
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let symb = bracketsConfig[i];
+        openingBracket.push(symb[0]);
+        closingBracket.push(symb[1]);
+    }
+    
+    for (let i = 0; i < string.length; i++) {
+      openSymb = openingBracket.indexOf(string[i]);
+      closeSymb = closingBracket.indexOf(string[i]);
+      if (openSymb !== closeSymb) {
+      if (openSymb !== -1) {
+        stack.push(openSymb);
+      }
+     
+      if (closeSymb !== -1) {
+        openSymb = stack.pop();
+        if (closeSymb !== openSymb) {
+          return false;
+        }
+      }
+     }
+     let lastBracket = stack[stack.length - 1];
+         if (lastBracket === undefined) {
+             stack.push(openSymb);
+             continue;
+         }
+         if ((lastBracket === openSymb) && (closeSymb !== -1)) {
+             openSymb = stack.pop();
+              if (closeSymb !== openSymb) {
+          return false;
+         }
+     } else {
+         stack.push(openSymb);
+     }
+    }
+    if (stack.length !== 0) {
       return false;
     }
-  }
-}
-if (stack.length !== 0) {
-  return false;
-}
-return true;
-}
+    return true;
+    }
